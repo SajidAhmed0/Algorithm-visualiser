64c159c1ab077a153f24f76b5d9de4a316876eb6
IT21294570 <mjsahmed205@gmail.com>
2023-07-23 01:04:18 +0530

    basic setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,21 @@
-console.log("hello world");
-console.log("I'm algorithm visualiser");
+import express from 'express';
+const app = express();
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB from "./db/connect.js";
+
+const port = process.env.PORT || 8090;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
